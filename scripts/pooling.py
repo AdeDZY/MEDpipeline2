e481fa7b0a662c4ca42677150bef3cd1adb4d2fb
zhuyund <zhuyund@cs.cmu.edu>
2016-02-15 21:12:45 -0500

    add max pooling

--- scripts/pooling.py
+++ scripts/pooling.py
@@ -9,10 +9,11 @@ import argparse
 from os import listdir
 from os.path import isfile, join
 
+
 def avg_feats(feat_file_path):
     """
-    Generate the average of MFCC features
-    :param mfcc_file_path
+    Generate the average of features
+    :param feat_file_path
     :return: vec
     """
     print feat_file_path
@@ -34,11 +35,31 @@ def avg_feats(feat_file_path):
     return vec
 
 
+def max_feats(feat_file_path):
+    """
+    Generate the max of features
+    :param feat_file_path
+    :return: vec
+    """
+    print feat_file_path
+    vec = []
+    for line in open(feat_file_path):
+        vals = line.split(';')
+        for i, v in enumerate(vals):
+            v = float(v)
+            if len(vec) <= i:
+                vec.append(v)
+            else:
+                vec[i] = max(vec[i], v)
+
+    return vec
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("feat_dir", help="the dir of video features")
     parser.add_argument("list_file", help="a list of all videos")
     parser.add_argument("output_file_path", help="the output file")
+    parser.add_argument("--method", '-m', help="pooling method", choices=["avg", "max"], default='avg')
     args = parser.parse_args()
 
     # output file
@@ -60,7 +81,12 @@ def main():
             output_str = vid + '\t-1\n'
             output_file.write(output_str)
             continue
-        vec = avg_feats(vid2feat[vid])
+
+        if args.method == 'avg':
+            vec = avg_feats(vid2feat[vid])
+        else:
+            vec = max_feats(vid2feat[vid])
+
         output_str = ';'.join([str(t) for t in vec])
         output_file.write(vid + '\t')
         output_file.write(output_str + '\n')
