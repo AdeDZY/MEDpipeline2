5d4a2d8e30b5bb2e6fe09031784390f46e00f315
zhuyun <daizhuyun@gmail.com>
2016-02-22 03:19:59 +0000

    cleaned up run.feature and run.test

--- run.feature.sh
+++ run.feature.sh
@@ -8,25 +8,31 @@ export LD_LIBRARY_PATH=$ffmpeg_path/libs:$opensmile_path/lib:$LD_LIBRARY_PATH
 
 video_path=../video   # path to the directory containing all the videos. In this example setup, we are linking all the videos to "../video"
 
-#for line in $(cat "list/all.video"); 
-#do
-#    ffmpeg -y -ss 0 -i $video_path/${line}.mp4 -strict experimental -t 30 -r 15 -vf scale=224x224,setdar=1:1 video2/${line}.mp4
-#    ffmpeg -ss 0 -i video/${line}.mp4 -t 30  -vf select="eq(pict_type\,PICT_TYPE_I)" -vsync 0 keyframes2/${line}_%03d.jpg
-#done
+for line in $(cat "list/all.video"); 
+do
+    # key frames for sift: 160x120
+    ffmpeg -y -ss 0 -i $video_path/${line}.mp4 -strict experimental -t 30 -r 15 -vf scale=160x120,setdar=4:3 video/${line}.mp4
+    ffmpeg -ss 0 -i video/${line}.mp4 -t 30  -vf select="eq(pict_type\,PICT_TYPE_I)" -vsync 0 keyframes/${line}_%03d.jpg
 
+    # key frames for cnn: 224x224 
+    ffmpeg -y -ss 0 -i $video_path/${line}.mp4 -strict experimental -t 30 -r 15 -vf scale=224x224,setdar=1:1 video2/${line}.mp4
+    ffmpeg -ss 0 -i video/${line}.mp4 -t 30  -vf select="eq(pict_type\,PICT_TYPE_I)" -vsync 0 keyframes2/${line}_%03d.jpg
+done
 
-#for video in $(cat "list/all.video"); 
-#do
-#    echo ${video}
-#    i=0
-#    for line in $(ls ~/hw2/keyframes/${video}_*);
-#    do
-#        let i=i+1
-#        ~/hw2/scripts/extractSift ${line} > ./sift_features/${video}_$(printf "%03d" $i).sift
-#    done
-#done 
+# SIFT
+for video in $(cat "list/all.video"); 
+do
+    echo ${video}
+    i=0
+    for line in $(ls ~/hw2/keyframes/${video}_*);
+    do
+        let i=i+1
+        ~/hw2/scripts/extractSift ${line} > ./sift_features/${video}_$(printf "%03d" $i).sift
+    done
+done 
 
     
+# cnn features (use FC7 layer)
 for video in $(cat "list/all.video"); 
 do
     echo ${video}
--- run.test.sh
+++ run.test.sh
@@ -1,36 +1,23 @@
-#for i in {1..3};do
-#./scripts/train_svm.py P00${i} siftbow_features/all_max.vectors 500 sift_pred/P00${i}_max.model -g 0.01 -k rbf 
-#./scripts/test_svm.py sift_pred/P00${i}_max.model siftbow_features/all_max.vectors 500 sift_pred/P00${i}_max.pred
-#../hw1/mAP/ap list/P00${i}_test_label sift_pred/P00${i}_max.pred
-#done
+# Sift RBF
+g=(0.1 0.005 0.00001)
+for i in {1..3};do
+./scripts/train_svm.py P00${i} siftbow_features/all_avg.vectors 500 sift_pred/P00${i}_avg.model -g ${g[i-1]} -k rbf 
+./scripts/test_svm.py sift_pred/P00${i}_avg.model siftbow_features/all_avg.vectors 500 sift_pred/P00${i}_avg.pred
+../hw1/mAP/ap list/P00${i}_test_label sift_pred/P00${i}_avg.pred >> log
+done
 
-#for i in {1..3};do
-#./scripts/test_svm.py sift_pred/P00${i}_k500.model siftbow_features/all_max.vectors 500 sift_pred/P00${i}.pred
-#../hw1/mAP/ap list/P00${i}_test_label sift_pred/P00${i}_k500.pred
-#done
-
-#./scripts/train_svm.py P001 cnn_features/all_avg.vectors 1000 cnn_pred/P001.model -k rbf -g 0.0002
-#./scripts/test_svm.py cnn_pred/P001.model cnn_features/all_avg.vectors 1000 cnn_pred/P001.pred
-#../hw1/mAP/ap list/P001_test_label cnn_pred/P001.pred
-#
-#./scripts/train_svm.py P002 cnn_features/all_avg.vectors 1000 cnn_pred/P002.model -k rbf -g 0.0001
-#./scripts/test_svm.py cnn_pred/P002.model cnn_features/all_avg.vectors 1000 cnn_pred/P002.pred
-#../hw1/mAP/ap list/P002_test_label cnn_pred/P002.pred
-#
-#./scripts/train_svm.py P003 cnn_features/all_avg.vectors 1000 cnn_pred/P003.model -k rbf -g 0.001
-#./scripts/test_svm.py cnn_pred/P003.model cnn_features/all_avg.vectors 1000 cnn_pred/P003.pred
-#../hw1/mAP/ap list/P003_test_label cnn_pred/P003.pred
-
+# CNN RBF
 g=(0.0002 0.0001 0.001)
-i=0
 for i in {1..3};do
 ./scripts/train_svm.py P00${i} cnn_fc7_features/all_avg.vectors 4096 cnn_pred/fc7_P00${i}.model -k rbf -g ${g[i-1]} 
 ./scripts/test_svm.py cnn_pred/fc7_P00${i}.model cnn_fc7_features/all_avg.vectors 4096 cnn_pred/fc7_P00${i}.pred
-../hw1/mAP/ap list/P00${i}_test_label cnn_pred/fc7_P00${i}.pred
+../hw1/mAP/ap list/P00${i}_test_label cnn_pred/fc7_P00${i}.pred >> log
 done
 
 
-#for i in {1..3};do
-#./scripts/test_svm.py cnn_pred/P00${i}.model cnn_features/all.vectors 100 cnn_pred/P00${i}.pred
-#../hw1/mAP/ap list/P00${i}_test_label cnn_pred/P00${i}.pred
-#done
+# Imtraj linear 
+for i in {1..3}; do
+./scripts/train_svm.py P00${i} imtraj 32748 imtraj_pred/P00${i}.model -f imtraj 
+./scripts/test_svm.py imtraj_pred/P00${i}.model imtraj 32748 imtraj_pred/P00${i}.pred -f imtraj
+../hw1/mAP/ap list/P00${i}_test_label imtraj_pred/P00${i}.pred >> log
+done
